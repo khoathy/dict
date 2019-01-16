1dc74c08f8be80e451aef777bb8edfb01c6da79c
KhoaThy <ms.khoathy@gmail.com>
2019-01-15 20:53:39 -0800

    character animation added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-$(window).scroll(function(){
-    console.log('hi');
+// $(window).scroll(function(){
+//     console.log('hi');
+// });
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('book-1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-book1.json'
 });
+
+var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('book-2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data-book2.json'
+})
+
