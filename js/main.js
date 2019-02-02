8923d7e666fa3aab6a173cac8ebff6f7c4e06d95
KhoaThy <ms.khoathy@gmail.com>
2019-02-01 23:02:16 -0800

    tab added

--- js/main.js
+++ js/main.js
@@ -18,3 +18,31 @@ var animation2 = bodymovin.loadAnimation({
     path: 'data-book2.json'
 })
 
+
+//tab
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabContent, tabLink;
+  
+    // Get all elements with class="tabContent" and hide them
+    tabContent = document.getElementsByClassName("result__tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tabLink = document.getElementsByClassName("result__tab-link");
+    for (i = 0; i < tabLink.length; i++) {
+      tabLink[i].className = tabLink[i].className.replace(" active", "");
+      
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+  }
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+    
